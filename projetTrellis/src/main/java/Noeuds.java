27cefec776d2402377868e19a067dfa034837d75
Aurel512 <82666775+Aurel512@users.noreply.github.com>
2021-04-16 17:52:00 +0200

    Update Noeuds.java

--- projetTrellis/src/main/java/Noeuds.java
+++ projetTrellis/src/main/java/Noeuds.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package com.mycompany.projettreillis;
 
-/**
- *
- * @author PC
- */
-public class Noeuds {
+public class Noeud {
     int identificateur;
     
-    public Noeuds (int identificateur){
+    public Noeud (int identificateur){
         this.identificateur = identificateur;
         
         
