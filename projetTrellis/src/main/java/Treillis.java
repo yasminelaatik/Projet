92a94fdd984a717dec5dfb7ed7696060a33c5a7e
Aurel512 <82666775+Aurel512@users.noreply.github.com>
2021-04-16 17:53:23 +0200

    Update Treillis.java

--- projetTrellis/src/main/java/Treillis.java
+++ projetTrellis/src/main/java/Treillis.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package com.mycompany.projettreillis;
 
-/**
- *
- * @author PC
- */
+
 public class Treillis {
-    
-    
+
 }
