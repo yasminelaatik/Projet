62a05ec932db548582f7581fe78fb144631f5b06
Aurel512 <82666775+Aurel512@users.noreply.github.com>
2021-04-16 17:42:20 +0200

    Update Catalogue.java

--- projetTrellis/src/main/java/Catalogue.java
+++ projetTrellis/src/main/java/Catalogue.java
@@ -1,28 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author PC
- */
+package com.mycompany.projettreillis;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class Catalogue { 
     
-    public static void main(String[] args) {
+    public static HashMap<Integer, String> type() {
         HashMap<Integer, String> type = new HashMap<>() ; 
         type.put(1, "acier") ;
         type.put(2, "bois") ;
         type.put(3, "aluminium") ;
         type.put(4, "béton") ;
         type.put(5, "bronze") ;
-        for(int i=1 ; i<=5 ; i++){
-            System.out.println(type.get(i));
-        }        
+        return type;
+    }
+        
+    public static HashMap<String, List<Double>> attributs() {
+        HashMap<String, List<Double>> attributs = new HashMap<>() ; 
+        List<Double> acier = Arrays.asList(35.0, 2.42, 82.3, 89.75, 75.36);
+        List<Double> bois = Arrays.asList(35.0, 2.42, 82.3, 89.75, 75.36);
+        List<Double> aluminium = Arrays.asList(35.0, 2.42, 82.3, 89.75, 75.36);
+        List<Double> béton = Arrays.asList(35.0, 2.42, 82.3, 89.75, 75.36);
+        List<Double> bronze = Arrays.asList(35.0, 2.42, 82.3, 89.75, 75.36);
+        attributs.put("acier", acier) ;
+        attributs.put("bois", bois) ;
+        attributs.put("aluminium", aluminium) ;
+        attributs.put("béton", béton) ;
+        attributs.put("bronze", bronze) ;
+        return attributs;
     }
 }
 
